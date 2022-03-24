475265f19559d04f30739f1bed1bd46f06321f46
nedzie <marcos-elivelton@hotmail.com>
2022-03-24 16:06:33 -0300

    Att

--- PeriodoAtras.ConsoleApp1/Program.cs
+++ PeriodoAtras.ConsoleApp1/Program.cs
@@ -6,12 +6,12 @@ namespace PeriodoAtras.ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Data datas = new();
+            Data datas = new(); // comment
             TimeSpan dias = new TimeSpan();
-           datas.dateHoje = DateTime.Now;
+            datas.dateHoje = DateTime.Now;
             datas.dataInformada = DateTime.Parse("14/02/2021");
             dias = datas.dateHoje - datas.dataInformada;
-           Console.WriteLine(datas.Mensagen(dias));
+            Console.WriteLine(datas.Mensagen(dias));
             Console.ReadKey();
         }
     }
