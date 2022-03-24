7d966352f8c1391c9c91f3f69415dc0caa9b3650
ALUNO <ALUNO@DESKTOP-T0UVCS1>
2022-03-24 16:03:26 -0300

    Add project files.

---
+++ PeriodoAtras.ConsoleApp1/Data.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PeriodoAtras.ConsoleApp1
+{
+    public class Data
+        {
+          public  DateTime dateHoje;
+           public DateTime dataInformada;
+
+        public string Mensagen(TimeSpan dias)
+        {
+            if (dias.Days<7)
+            {
+               return ($"{dias.Days} dias se passaram");
+            }
+           else if (dias.Days >= 7&& dias.Days < 30)
+            {
+               return ($"{dias.Days/7} semandas se passaram e {dias.Days % 7} dia(s)");
+            }
+              else if (dias.Days >= 30 && dias.Days < 360)
+            {
+                return ($"{dias.Days / 30} meses se passaram e {(dias.Days % 30)/7} semanas e {((dias.Days % 30) % 7)} dia(s)");
+            }
+            else if (dias.Days >= 360)
+            {
+                return ($"{dias.Days / 360} anos se passaram e {dias.Days % 360/30} meses e {(dias.Days % 360) % 30 / 7} semanas e {((dias.Days % 360) / 30 % 7)} dia(s)");
+            }
+            return "";
+        }
+    }
+  
+    
+}
+
+
---
+++ PeriodoAtras.ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PeriodoAtras.ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Data datas = new();
+            TimeSpan dias = new TimeSpan();
+           datas.dateHoje = DateTime.Now;
+            datas.dataInformada = DateTime.Parse("14/02/2021");
+            dias = datas.dateHoje - datas.dataInformada;
+           Console.WriteLine(datas.Mensagen(dias));
+            Console.ReadKey();
+        }
+    }
+}
+
+
