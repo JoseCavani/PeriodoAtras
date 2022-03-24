6a5a599244fb117b1476c276f43a8e00e6ed2e36
ALUNO <ALUNO@DESKTOP-T0UVCS1>
2022-03-24 16:41:34 -0300

    commit

--- PeriodoAtras.ConsoleApp1/Data.cs
+++ PeriodoAtras.ConsoleApp1/Data.cs
@@ -12,19 +12,29 @@ namespace PeriodoAtras.ConsoleApp1
         {
             if (dias.Days<7)
             {
-               return ($"{dias.Days} dias se passaram");
+                A dia = (A)dias.Days;
+                return ($"{dia} dias se passaram");
             }
            else if (dias.Days >= 7&& dias.Days < 30)
             {
-               return ($"{dias.Days/7} semandas se passaram e {dias.Days % 7} dia(s)");
+                A dia = (A)(dias.Days % 7);
+                A semanas = (A)(dias.Days / 7);
+                return ($"{semanas} semandas se passaram e {dia} dia(s)");
             }
               else if (dias.Days >= 30 && dias.Days < 360)
             {
-                return ($"{dias.Days / 30} meses se passaram e {(dias.Days % 30)/7} semanas e {((dias.Days % 30) % 7)} dia(s)");
+                A dia = (A)(((dias.Days % 30) % 7));
+                A semanas = (A)((dias.Days % 30) / 7);
+                A meses = (A)(dias.Days / 30);
+                return ($"{meses} meses se passaram e {semanas} semanas e {dia} dia(s)");
             }
             else if (dias.Days >= 360)
             {
-                return ($"{dias.Days / 360} anos se passaram e {dias.Days % 360/30} meses e {(dias.Days % 360) % 30 / 7} semanas e {((dias.Days % 360) / 30 % 7)} dia(s)");
+                A dia = (A)(((dias.Days % 360) / 30 % 7));
+                A semanas = (A)((dias.Days % 360) % 30 / 7);
+                A meses = (A)(dias.Days % 360 / 30);
+                A anos = (A)(dias.Days / 360);
+                return ($"{dia} anos se passaram e {semanas} meses e {meses} semanas e {anos} dia(s)");
             }
             return "";
         }
--- PeriodoAtras.ConsoleApp1/Program.cs
+++ PeriodoAtras.ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ namespace PeriodoAtras.ConsoleApp1
             Data datas = new();
             TimeSpan dias = new TimeSpan();
            datas.dateHoje = DateTime.Now;
-            datas.dataInformada = DateTime.Parse("23/03/2022");
+            datas.dataInformada = DateTime.Parse("10/03/2022");
             dias = datas.dateHoje - datas.dataInformada;
            Console.WriteLine(datas.Mensagen(dias));
             Console.ReadKey();
