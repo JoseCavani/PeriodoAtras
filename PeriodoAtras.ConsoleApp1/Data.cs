c440091381fb82deecad2175a366b2c600b9546a
ALUNO <ALUNO@DESKTOP-T0UVCS1>
2022-03-24 17:14:28 -0300

    commit

--- PeriodoAtras.ConsoleApp1/Data.cs
+++ PeriodoAtras.ConsoleApp1/Data.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.ComponentModel;
 
 namespace PeriodoAtras.ConsoleApp1
 {
-    public class Data
+    public partial class Data
     {
         public DateTime dateHoje;
         public DateTime dataInformada;
@@ -12,46 +11,88 @@ namespace PeriodoAtras.ConsoleApp1
         {
             if (dias.Days < 7)
             {
-                A dia = (A)dias.Days;
-                return ($"{dia} dias se passaram");
+                IntParaStringDias dia = (IntParaStringDias)dias.Days;
+                if (dias.Days > 1)
+                {
+                    return ($"{dia} dia se passaram");
+                }
+                else
+                    return ($"{dia} dias se passaram");
             }
             else if (dias.Days >= 7 && dias.Days < 30)
             {
-                A dia = (A)(dias.Days % 7);
-                A semanas = (A)(dias.Days / 7);
-                return ($"{semanas} semandas se passaram e {dia} dia(s)");
+                IntParaStringDias semanas;
+                IntParaStringDias dia = (IntParaStringDias)(dias.Days % 7);
+                if (dias.Days / 7 == 2)
+                    semanas = (IntParaStringDias)Enum.Parse(typeof(IntParaStringDias), "duas");
+
+                else
+                    semanas = (IntParaStringDias)(dias.Days / 7);
+
+                if (dias.Days / 7 == 0)
+                {
+                    return ($" se passaram {semanas} semana(s)");
+                }
+                return ($"se passaram {semanas} semana(s) e {dia} dia(s)");
             }
             else if (dias.Days >= 30 && dias.Days < 360)
             {
-                A dia = (A)(((dias.Days % 30) % 7));
-                A semanas = (A)((dias.Days % 30) / 7);
-                A meses = (A)(dias.Days / 30);
-                return ($"{meses} meses se passaram e {semanas} semanas e {dia} dia(s)");
+                IntParaStringDias dia = (IntParaStringDias)(((dias.Days % 30) % 7));
+                IntParaStringDias semanas;
+                IntParaStringDias meses = (IntParaStringDias)(dias.Days / 30);
+
+
+                if ((dias.Days % 30) / 7 == 2)
+                    semanas = (IntParaStringDias)Enum.Parse(typeof(IntParaStringDias), "duas");
+
+                else
+                    semanas = (IntParaStringDias)(dias.Days / 7);
+
+
+                if ((dias.Days % 30) / 7 == 0)
+                {
+                    if ((dias.Days % 30) / 7 == 0)
+                    {
+                        return ($"se passaram {meses} mes(es)");
+                    }
+                    return ($"se passaram {meses} mes(es) e {semanas} semana(s)");
+                }
+
+
+                return ($"{meses} mese(s) se passaram e {semanas} semana(s) e {dia} dia(s)");
             }
             else if (dias.Days >= 360)
             {
-                A dia = (A)(((dias.Days % 360) / 30 % 7));
-                A semanas = (A)((dias.Days % 360) % 30 / 7);
-                A meses = (A)(dias.Days % 360 / 30);
-                A anos = (A)(dias.Days / 360);
-                return ($"{dia} anos se passaram e {semanas} meses e {meses} semanas e {anos} dia(s)");
+                IntParaStringDias dia = (IntParaStringDias)(((dias.Days % 360) / 30 % 7));
+                IntParaStringDias semanas = (IntParaStringDias)((dias.Days % 360) % 30 / 7);
+                IntParaStringDias meses = (IntParaStringDias)(dias.Days % 360 / 30);
+                IntParaStringDias anos = (IntParaStringDias)(dias.Days / 360);
+
+                if ((dias.Days % 360) % 30 / 7 == 2)
+                    semanas = (IntParaStringDias)Enum.Parse(typeof(IntParaStringDias), "duas");
+
+                else
+                    semanas = (IntParaStringDias)((dias.Days % 360) % 30 / 7);
+
+
+
+
+                if ((dias.Days % 360) / 30 % 7 == 0 && (dias.Days % 360) % 30 / 7 == 0 && (dias.Days % 360 / 30) == 0)
+
+                    return ($" se passaram {anos} ano(s)");
+
+                else if ((dias.Days % 360) / 30 % 7 == 0 && (dias.Days % 360) % 30 / 7 == 0)
+                    return ($" se passaram {anos} ano(s) e {meses} mese(s)");
+                else if ((dias.Days % 360) / 30 % 7 == 0)
+                    return ($"se passaram {dia} ano(s) e {meses} mese(s) e {semanas} semana(s)");
+
+                return ($"se passaram {anos} ano(s) e {meses} mese(s) e {semanas} semana(s) e {dia} dia(s)");
             }
             return "";
         }
-        public enum A
-        {
-            [Description("zero")]
-            zero = 0,
-            [Description("um")]
-            um = 1,
-            [Description("dois")]
-            dois = 2
-
-
-        }
     }
-  
-    
+
+
 }
 
 
---
+++ PeriodoAtras.ConsoleApp1/IntParaStringDias.cs
@@ -0,0 +1,81 @@
+﻿using System.ComponentModel;
+
+namespace PeriodoAtras.ConsoleApp1
+{
+    public partial class Data
+    {
+        public enum IntParaStringDias
+        {
+            [Description("zero")]
+            zero = 0,
+            [Description("um")]
+            um = 1,
+            [Description("dois")]
+            dois = 2,
+            [Description("duas")]
+            duas = 2,
+            [Description("três")]
+            tres = 3,
+            [Description("quatro")]
+            quatro = 4,
+            [Description("cinco")]
+            cinco = 5,
+             [Description("seis")]
+            seis = 6,
+            [Description("sete")]
+            sete = 7,
+            [Description("oito")]
+            oito = 8,
+            [Description("nove")]
+            nove = 9,
+            [Description("dez")]
+            dez = 10,
+            [Description("onze")]
+            onze = 11,
+            [Description("doze")]
+            doze = 12,
+            [Description("treze")]
+            treze = 13,
+            [Description("catorze")]
+            catorze = 14,
+            [Description("quinze")]
+            quinze = 15,
+            [Description("dezesseis")]
+            dezesseis = 16,
+            [Description("dezessete")]
+            dezessete = 17,
+            [Description("dezoito")]
+            dezoito = 18,
+            [Description("dezenove")]
+            dezenove = 19,
+            [Description("vinte")]
+            vinte = 20,
+            [Description("vinte e um")]
+            vinteeum = 21,
+            [Description("vinte e dois")]
+            vinteedois = 22,
+            [Description("vinte e três")]
+            vinteetres = 23,
+            [Description("vinte e quatro")]
+            vinteequatro = 24,
+            [Description("vinte e cinco")]
+            vinteecinco = 25,
+            [Description("vinte e seis")]
+            vinteeseis = 26,
+            [Description("vinte e sete")]
+            vinteesete = 27,
+            [Description("vinte e oito")]
+            vinteeoito = 28,
+            [Description("vinte e nove")]
+            vinteenove = 29,
+            [Description("trinta")]
+            trinta = 30,
+            [Description("trinta e um")]
+            trintaeum = 31
+        }
+    }
+  
+    
+}
+
+
