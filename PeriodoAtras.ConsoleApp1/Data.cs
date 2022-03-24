651c76be050a3aa5a70c0e927a4b0bab6f62fc51
nedzie <marcos-elivelton@hotmail.com>
2022-03-24 16:32:26 -0300

    +87987

--- PeriodoAtras.ConsoleApp1/Data.cs
+++ PeriodoAtras.ConsoleApp1/Data.cs
@@ -4,41 +4,30 @@ using System.ComponentModel;
 namespace PeriodoAtras.ConsoleApp1
 {
     public class Data
-        {
-          public  DateTime dateHoje;
-           public DateTime dataInformada;
+    {
+        public DateTime dateHoje;
+        public DateTime dataInformada;
 
         public string Mensagen(TimeSpan dias)
         {
-            if (dias.Days<7)
+            if (dias.Days < 7)
             {
-               return ($"{dias.Days} dias se passaram");
+                return ($"{dias.Days} dias se passaram");
             }
-           else if (dias.Days >= 7&& dias.Days < 30)
+            else if (dias.Days >= 7 && dias.Days < 30)
             {
-               return ($"{dias.Days/7} semandas se passaram e {dias.Days % 7} dia(s)");
+                return ($"{dias.Days / 7} semandas se passaram e {dias.Days % 7} dia(s)");
             }
-              else if (dias.Days >= 30 && dias.Days < 360)
+            else if (dias.Days >= 30 && dias.Days < 360)
             {
-                return ($"{dias.Days / 30} meses se passaram e {(dias.Days % 30)/7} semanas e {((dias.Days % 30) % 7)} dia(s)");
+                return ($"{dias.Days / 30} meses se passaram e {(dias.Days % 30) / 7} semanas e {((dias.Days % 30) % 7)} dia(s)");
             }
             else if (dias.Days >= 360)
             {
-                return ($"{dias.Days / 360} anos se passaram e {dias.Days % 360/30} meses e {(dias.Days % 360) % 30 / 7} semanas e {((dias.Days % 360) / 30 % 7)} dia(s)");
+                return ($"{dias.Days / 360} anos se passaram e {dias.Days % 360 / 30} meses e {(dias.Days % 360) % 30 / 7} semanas e {((dias.Days % 360) / 30 % 7)} dia(s)");
             }
             return "";
         }
-    }
-    public enum A 
-    {
-        [Description("zero")]
-        zero = 0,
-        [Description("um")]
-        um = 1,
-        [Description("dois")]
-        dois = 2
-
-
     }
   
     
